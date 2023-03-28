8e6edee6ccc49412ad98bdd1433f178fded46bd5
Daniel Shanahan <daniel.patrick.shanahan@gmail.com>
2023-03-28 13:53:23 -0400

    Styled SignIn

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 function SignIn({ props }) {
   const {firebase, auth, db} = props;
 
@@ -10,13 +12,21 @@ function SignIn({ props }) {
   };
 
   return (
-    <div className="bg-gray-50 h-full flex flex-col items-center gap-32">
-      <h1 className="text-blue-500 font-semibold text-5xl text-center flex-wrap">Join the conversation on Connector!</h1>
-      <button 
-        className="bg-blue-500 text-white px-6 py-3 rounded-full font-semibold"
-        onClick={signInWithGoogle}>
-        Sign in with Google
-      </button>
+    <div className='bg-gray-50 h-screen flex items-center justify-center'>
+      <div className="w-[500px] flex flex-col items-center gap-28">
+        <div className='text-blue-500 font-semibold text-3xl text-center'>
+          <FontAwesomeIcon icon='share-nodes' className='px-1' />
+          <h1 className='inline'>Connector</h1>
+        </div>
+        <p className="text-gray-900 text-4xl text-center">
+          Connect with <p className='font-semibold inline'>friends</p> and the <p className='font-semibold inline'>world</p> around you.
+        </p>
+        <button 
+          className="bg-blue-500 text-white px-6 py-3 rounded-full font-semibold"
+          onClick={signInWithGoogle}>
+          Sign in with Google
+        </button>
+      </div>
     </div>
   );
 }
