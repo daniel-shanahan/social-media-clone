9f0e9f50fd99dc96c18c69c66251026ebc56cf10
Daniel Shanahan <daniel.patrick.shanahan@gmail.com>
2023-04-09 14:54:10 -0400

    Top bar sticks to top of view screen

--- src/TopBar.js
+++ src/TopBar.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 function TopBar({ auth }) {  
   return (
-    <nav className="h-14 w-full bg-blue-500 text-white font-semibold shadow-md flex justify-end text-xl gap-4 pr-4 sm:gap-8 sm:pr-8">
+    <nav className="sticky top-0 h-14 w-full bg-blue-500 text-white font-semibold shadow-md flex justify-end text-xl gap-4 pr-4 sm:gap-8 sm:pr-8">
       <NavLink to='/feed' className='text-2xl my-3 absolute left-4 sm:left-8'>Connector</NavLink>
       <NavLink to='/feed' className='my-auto'>
         <FontAwesomeIcon icon='house' className='px-1' />
