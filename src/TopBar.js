88ffebb1b361a384d5207d8f80f18e284f23cf5b
Daniel Shanahan <daniel.patrick.shanahan@gmail.com>
2023-04-30 18:17:37 -0400

    Navigate to home page after sign out.

--- src/TopBar.js
+++ src/TopBar.js
@@ -1,7 +1,9 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 function TopBar({ auth }) {     
+  const navigate = useNavigate();
+
   return (
     <nav className="sticky top-0 h-14 w-full bg-blue-500 text-white font-semibold shadow-md flex justify-end text-xl gap-4 pr-4 sm:gap-8 sm:pr-8">
       <NavLink to='/feed' className='text-2xl my-3 absolute left-4 sm:left-8'>Connector</NavLink>
@@ -11,7 +13,10 @@ function TopBar({ auth }) {
       <NavLink to={'/profile/' + auth.currentUser.uid} className='my-auto'>
         <FontAwesomeIcon icon='user' className='px-1' />
       </NavLink>
-      <button onClick={() => auth.signOut()}>
+      <button onClick={() => {
+          auth.signOut();
+          navigate('/', {replace: true});
+        }}>
         <FontAwesomeIcon icon='right-from-bracket' className='px-1' />
       </button>
     </nav>
